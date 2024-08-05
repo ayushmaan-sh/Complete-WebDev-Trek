5d757cfd2f707bee2b09f10c6ae1c62dd4437b1f
Ayushmaan Shukla <ayushmaanshukla274@gmail.com>
2024-08-05 22:57:15 +0530

    Data types and conversions

---
+++ 02_JavaScript/data_types.js
@@ -0,0 +1,25 @@
+
+let number = 42;             // Number
+let string = "Hello World";  // String
+let isActive = false;        // Boolean
+let numbers = [1, 2, 3];     // Array
+
+
+// Datatype conversion
+
+// String to number
+let score = "33"
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+
+// Number to Boolean
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof isLoggedIn)
+
+// if empty, false
+
+// And more like this, we can do as many conversions
