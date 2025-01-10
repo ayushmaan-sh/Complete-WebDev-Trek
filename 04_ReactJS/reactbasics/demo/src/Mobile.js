14c9c4ed4c3bae4008b278c500471ad18959321a
Ayushmaan Shukla <ayushmaanshukla274@gmail.com>
2025-01-10 09:17:06 +0530

    learned about useState hook

--- 04_ReactJS/reactbasics/demo/src/Mobile.js
+++ 04_ReactJS/reactbasics/demo/src/Mobile.js
@@ -1,16 +1,34 @@
-import './App.css';
-import Mobile from './Mobile';
-import mobile from './mobile.json'
+import { useState } from 'react';
 
 
-function App() {
-  return (
-    <div class="App">
-      {mobile.map((element)=>{
-        return <Mobile image={element.image} mobileName={element.mobileName} price={element.price} />
-      })}
-    </div>
-  );
+export default function Mobile(props) {
+    const { image, mobileName, price } = props;
+
+    // Using state for counter
+    const [counter, setCounter] = useState(0);
+
+    const addValue = () => {
+        setCounter(counter + 1);
+    };
+
+    const removeValue = () => {
+        if(counter === 0){
+            return alert('cannot be less then 0!')
+        }
+        setCounter(counter - 1)
+    }
+
+    return (
+        <div id="mobileCard">
+            <div className='card'>
+                <img src={image} alt="mobile" />
+                <p>{mobileName}</p>
+                <p>{price}</p>
+                <h4>Cart Value: {counter}</h4>
+                <button onClick={addValue}>Add to cart</button>
+                <br />
+                <button onClick={removeValue}>Remove from cart</button>
+            </div>
+        </div>
+    );
 }
-
-export default App;
