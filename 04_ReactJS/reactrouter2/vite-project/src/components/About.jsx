f058c850f696e76f412c3a589ba7b8844a317f53
Ayushmaan Shukla <ayushmaanshukla274@gmail.com>
2025-01-16 09:06:46 +0530

    About route

---
+++ 04_ReactJS/reactrouter2/vite-project/src/components/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function About() {
+  return (
+    <>
+        <div class="text-center">
+            <h2 class="text-4xl font-bold mb-4">About Us</h2>
+            <p class="text-lg mb-6">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </div>
+    </>
+  )
+}
+
+export default About
