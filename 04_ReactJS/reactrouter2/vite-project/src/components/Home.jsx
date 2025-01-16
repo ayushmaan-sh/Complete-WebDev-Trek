48e7f0e3205c1280ab148d40dd4895087fe1980d
Ayushmaan Shukla <ayushmaanshukla274@gmail.com>
2025-01-16 09:06:14 +0530

    Home route

---
+++ 04_ReactJS/reactrouter2/vite-project/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+    <main class="container mx-auto py-16">
+        <div class="text-center">
+            <h2 class="text-4xl font-bold mb-4">Welcome to My Website</h2>
+            <p class="text-lg mb-6">This is a simple home page built with Tailwind CSS.</p>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Learn More</button>
+        </div>
+    </main>
+    </div>
+  )
+}
+
+export default Home
